9f9819983ca8edbef33f3fbcd61b79e766fcf2af
perqin <perqinxie@gmail.com>
2022-03-29 22:27:59 +0800

    feat: load waifu from file system

--- lib/business/harem.dart
+++ lib/business/harem.dart
@@ -1,18 +1,35 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/painting.dart';
 import 'package:guess_waifu_game/business/waifu.dart';
+import 'package:path/path.dart';
+
+var validImageExtensions = ['.png', '.jpg', '.jpeg', '.bmp'];
 
 class Harem {
+  final List<File> _unpickedList = [];
+  final List<File> _pickedList = [];
+  final _random = Random();
+
   init() async {
-    // Not implemented: Load a full list of waifu
-    throw Error();
+    var list = await Directory('./waifu')
+        .list()
+        .takeWhile((entity) => entity is File)
+        .map((entity) => entity as File)
+        .takeWhile((f) => validImageExtensions.contains(extension(f.path)))
+        .toList();
+    _unpickedList.addAll(list);
   }
 
   Future<Waifu> pickWaifu() async {
-    // Not implemented: Load image into waifu
-    return Waifu(
-      const NetworkImage('https://perqin.github.io/img/favicon.png'),
-      [],
-    );
+    var index = _random.nextInt(_unpickedList.length);
+    var picked = _unpickedList[index];
+    _unpickedList.removeAt(index);
+    _pickedList.add(picked);
+    var original = FileImage(picked);
+    // Not implemented: generate blur image
+    return Waifu(original, []);
   }
 }
 
